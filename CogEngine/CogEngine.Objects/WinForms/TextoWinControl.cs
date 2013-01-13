d419b4c2d4625281f46877aebe31a6276eadf997
Emerson Silveira Batista <emersonsilveirabatista@gmail.com>
2013-01-13 15:13:04 -0200

    CogEngine V0.31
    Nesta versão o problema com o controle texto, que não era exibido
    totalmente, foi corrigido.

--- CogEngine/CogEngine.Objects/WinForms/TextoWinControl.cs
+++ CogEngine/CogEngine.Objects/WinForms/TextoWinControl.cs
@@ -14,6 +14,7 @@ namespace CogEngine.Objects.WinForms
         {
             _Texto = concentradorTexto;
             Text = _Texto.Nome;
+            AutoSize = true;
         }
 
         [Browsable(false)]
