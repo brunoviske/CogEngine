59ae9e1128ad3026b363d6d9891be2e856ae0803
Emerson Silveira Batista <emersonsilveirabatista@gmail.com>
2013-03-20 23:56:48 -0300

    Classe Dicionario

---
+++ CogEngine/CogEngine.Objects/Dicionario.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CogEngine.Objects
+{
+    public class Dicionario<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        private void AdicionarChave(TKey chave)
+        {
+            if (!ContainsKey(chave))
+            {
+                Add(chave, default(TValue));
+            }
+        }
+
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                AdicionarChave(key);
+                return base[key];
+            }
+            set
+            {
+                AdicionarChave(key);
+                base[key] = value;
+            }
+        }
+    }
+}
